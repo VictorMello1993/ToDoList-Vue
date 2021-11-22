efa227defb934e023af728090172553a804c080e
Victor Mello <victorsmello93@gmail.com>
2021-11-21 22:56:14 -0300

    Implementados os componentes de barra de progresso e input text de adicionar nova tarefa

---
+++ src/components/NewTask.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="newTask">
+    <input type="text" class="inputText" placeholder="Nova tarefa?" />
+    <input type="button" class="addTask" value="+" />
+  </div>
+</template>
+<style scoped>
+input {  
+  border: solid #fff 1px;
+  color: #fff  
+}
+.inputText {
+  background-color: #555758;
+  outline: 0;  
+  height: 20px;
+  border-radius: 4px 0 0 4px;
+  width: 250px; 
+}
+::placeholder {
+  color: #c8cdcc;
+}
+.addTask {
+  background-color: #32BBF6;
+  color: #fff;
+  height: 24px;  
+  border-radius: 0 4px 4px 0;
+}
+
+.newTask{
+  margin-top: 30px;
+}
+</style>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="progress">0%</div>
+</template>
+<style scoped>
+.progress {
+  width: 90%;
+  border: 1px #fff solid;
+  border-radius: 4px;
+  text-align: center;
+}
+</style>
